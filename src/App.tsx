a5a22156934c7d2c60039a3b6784c5f6a107b327
artem_ <artem_5511@mail.ru>
2023-03-29 00:24:03 +0300

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from './Todolist';
+
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
+    const tasks: TasksType[] = [
+        {id: 1, title: "HTML & CSS", isDone: true},
+        {id: 2, title: "CSS & SCSS", isDone: true},
+        {id: 1, title: "ES6/TS", isDone: false},
+    ]
+
     return (
         <div className="App">
-            <div className="todolist">
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={"What to learn"}/>
+            {/*<Todolist title={"What to buy"}/>*/}
+            {/*<Todolist title={"What to learn"}/>*/}
         </div>
     );
 }
