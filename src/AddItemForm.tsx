3a6bfb9519374341dbfa482b44e1ec31aa6f9df5
artem_ <artem_5511@mail.ru>
2023-05-14 23:50:36 +0300

    newInput

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 const AddItemForm = () => {
     return (
         <div>
-            
+            <div>
+                <input className={error || isTitleLenghtTooLong? "input-error" : undefined}
+                       placeholder={'please, enter title'}
+                       value={newTitle}
+                       onKeyPress={onKeyPressHandler}
+                       onChange={onChangeHandler}/>
+                <button disabled={isAddBtnDisabled}
+                        onClick={addTaskHandler}>+
+                </button>
+                {titleMaxLenghtWarning}
+                {userMessage}
+
+            </div>
         </div>
     );
 };
